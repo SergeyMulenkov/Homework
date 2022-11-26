a3228d6fae67f73c797fc948464855fad4d3dfdf
Sergey Mulenkov <sergej.mulenkov@gmail.com>
2022-11-26 23:25:11 +0300

    Решение ДЗ после 4 семинара

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int num1 = new Random().Next(1,5);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1,5);
+Console.WriteLine(num2);
+Console.WriteLine($"Число {num1} в степени {num2}: " + exponentiation(num1, num2));
+
+int exponentiation(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = num1 * result;
+    }
+    return result;
+}
---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+RandomArray(array);
+PrintArray(array);
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
