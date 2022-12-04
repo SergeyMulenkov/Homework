c16b0d272597eee7dbd44b91f1550c8f054cc6ab
Sergey Mulenkov <sergej.mulenkov@gmail.com>
2022-12-04 17:02:55 +0300

    Решение ДЗ после шестого семинара

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите элементы(через пробел) и нажмите ENTER: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int numbers = Elements(arr);
+Console.WriteLine($"Количество элементов больше нуля: {numbers}");
+
+int Elements(int[] arr)
+{
+    int numbers = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) numbers++;
+    }
+    return numbers;
+}
