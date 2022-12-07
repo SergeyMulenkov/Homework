2f2fe3f97f021eabe27d50736d02fc087cab4366
Sergey Mulenkov <sergej.mulenkov@gmail.com>
2022-12-07 20:39:34 +0300

    Решение ДЗ после седьмого семинара

---
+++ seminar7/task47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[rows, colums];
+
+FillArray(array);
+Print(array);
+
+void FillArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().NextDouble();
+        }
+    }
+}
+
+void Print(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ seminar7/task52/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] creatMatrixRndInt = CreateMatrixRndInt(3, 4);
+PrintMatrix(creatMatrixRndInt);
+double[] array = ArithmeticMean(creatMatrixRndInt);
+Console.Write("Среднее арифметическое элементов в столбцах: ");
+PrintArray(array);
+
+
+int[,] CreateMatrixRndInt(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) 
+            {
+                Console.Write($"{matrix[i, j],4}, ");
+            }
+            else 
+            {
+                Console.Write($"{matrix[i, j],4} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] ArithmeticMean(int[,] matrix)
+{
+    double[] ArithmeticMeanNewMassiv = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sumArithmeticMean = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sumArithmeticMean += matrix[i, j];
+        }
+        ArithmeticMeanNewMassiv[j] = Math.Round(sumArithmeticMean / matrix.GetLength(0), 2);
+    }
+    return ArithmeticMeanNewMassiv;
+}
+
+void PrintArray(double[] arr)
+{
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) 
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+        else 
+        {
+            Console.Write($"{arr[i]}");
+        }
+    }
+}
+
+
+
