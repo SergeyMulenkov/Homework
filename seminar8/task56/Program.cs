50687f9107419cf15adf6a3135dbf011bfaededc
Sergey Mulenkov <sergej.mulenkov@gmail.com>
2022-12-10 16:50:21 +0300

    Решение ДЗ после восьмого семинара

---
+++ seminar8/task56/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] array = new int[4,4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int[] summaElementsRow = SummaElementsRow(array);
+(int, int) SmallestSummaRow = TheSmallestSummaRow(summaElementsRow);
+Console.WriteLine($"Минимальная сумма, строка: {SmallestSummaRow}");
+
+int[] SummaElementsRow(int[,] arr)
+{
+    int[] summaElements = new int[arr.GetLength(0)];
+    int summa = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            summa += array[i, j];
+            summaElements[i] = summa;
+        }
+        summa = 0;
+    }
+    return summaElements;
+}
+
+(int, int) TheSmallestSummaRow(int[] array)
+{
+    int resultSumma = array[0];
+    int resultRow = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < resultSumma)
+        {
+            resultSumma = array[i];
+            resultRow = i;
+        }
+    }
+    return (resultSumma, resultRow);
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(10, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ seminar8/task58/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] matrix = NewRndMatrix(3, 6);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] matrix2 = NewRndMatrix(6, 3);
+PrintMatrix(matrix2);
+Console.WriteLine();
+
+int[,] multiplyTwoMatrix = MultiplyTwoMatrix(matrix, matrix2);
+PrintMatrix(multiplyTwoMatrix);
+
+int[,] MultiplyTwoMatrix(int[,] matrix, int[,] matrix2)
+{
+    int[,] multiplyTwoMatrix = new int[matrix.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < multiplyTwoMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < multiplyTwoMatrix.GetLength(1); j++)
+            {
+                multiplyTwoMatrix[i, j] = 0;
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    multiplyTwoMatrix[i, j] += matrix[i, k] * matrix2[k, j];
+                }
+            }
+        }
+    return multiplyTwoMatrix;
+}
+
+int[,] NewRndMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+            {
+                Console.Write($"{matrix[i, j],4}, ");
+            }
+            else 
+            {
+                Console.Write($"{matrix[i, j],4}");
+            }
+        }
+        Console.WriteLine();
+    }
+}
